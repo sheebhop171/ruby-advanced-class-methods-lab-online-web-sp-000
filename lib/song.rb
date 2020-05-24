3f96894245e0b960fd4aaa6e60c45ae8b7c13cc7
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-24 17:28:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
   #  self.find_by_name(name) || self.create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
-    else 
+    else
       self.create_by_name(name)
     end
   end
