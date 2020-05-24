95784cc7852bab7a84779cdf5a56fc3009f8e2e7
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-24 17:28:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,12 @@ class Song
 
   def self.find_or_create_by_name(name)
 
-    self.find_by_name(name) || self.create_by_name(name)
-
+  #  self.find_by_name(name) || self.create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else 
+      self.create_by_name(name)
+    end
   end
 
   def self.alphabetical
